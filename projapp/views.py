4e3c08a32d6ffedcd4a12c28a0883f06b9f847a0
saov4000 <saov4000@gmail.com>
2024-04-22 17:03:25 -0300

    second commit

--- projapp/views.py
+++ projapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'app/home.html')
+
+def produtos(request):
+    return render(request,'app/produtos.html')
+
+def sobrenos(request):
+    return render(request,'app/sobrenos.html')
+
+def contato(request):
+    return render(request,'app/contato.html')
--- projeto02/urls.py
+++ projeto02/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from projapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name="home"),
+    path('produtos',views.produtos, name="produtos"),
+    path('sobrenos',views.sobrenos,name="sobrenos"),
+    path('contato',views.contato, name="contato")
 ]
